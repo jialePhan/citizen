33cd54622d843fd98ea3abb500b2ee6e730c4f4b
jialePhan <87068109+jialePhan@users.noreply.github.com>
2021-07-09 15:51:35 +0800

    Update db_connect.php

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect('localhost','root','3215484','wwwproject');
+$con = mysqli_connect('b1cm5xvdh8f01kkhz5wv-mysql.services.clever-cloud.com','u6ad0nkcvens50dg','D0cn1AxzbciSYbl7GNOA','b1cm5xvdh8f01kkhz5wv');
 if(!$con){
 	echo 'sql connect: '.mysqli_connect_error();
 }
